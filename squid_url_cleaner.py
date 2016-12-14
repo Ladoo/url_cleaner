bce093df2bbcf12d8eec8f812408a0ea88521d10
Andy Zaugg <andy.zaugg@gmail.com>
2016-12-13 21:36:39 -0800

    Handle signals for daemon processes, removed deprecated python var sub

--- squid_url_cleaner.py
+++ squid_url_cleaner.py
@@ -1,13 +1,23 @@
 #!/usr/bin/python
 
 import sys
+import signal
 from url_cleaner import removeBlackListedParameters
 
 
+def sig_handle(signal, frame):
+    sys.exit(0)
+
 while True:
-    line = sys.stdin.readline().strip()
-    urlList = line.split(' ')
-    urlInput = urlList[0]
-    newUrl = removeBlackListedParameters(urlInput)
-    sys.stdout.write('%s%s' % (newUrl, '\n'))
-    sys.stdout.flush()
+    signal.signal(signal.SIGINT, sig_handle)
+    signal.signal(signal.SIGTERM, sig_handle)
+
+    try:
+        line = sys.stdin.readline().strip()
+        urlList = line.split()
+        urlInput = urlList[0]
+        newUrl = removeBlackListedParameters(urlInput)
+        sys.stdout.write('%s%s'.format(newUrl, '\n'))
+        sys.stdout.flush()
+    except Exception:
+        continue
